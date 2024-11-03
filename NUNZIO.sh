c2c6da794b67ce1e2982f88b796c80f504adeb98
Rose-X <pizzardiPro@protonmail.com>
2024-11-03 11:20:17 +0100

    Update NUNZIO.sh
    MAJ

--- NUNZIO.sh
+++ NUNZIO.sh
@@ -375,17 +375,23 @@ NC='\033[0m' # Pas de couleur
 
 
 clear
-echo -e "\e[42m       \e[0m\e[47m       \e[0m\e[41m       \e[0m"
-echo -e "\e[42m       \e[0m\e[47m       \e[0m\e[41m       \e[0m"
-echo -e "\e[42m       \e[0m\e[47m       \e[0m\e[41m       \e[0m"
-echo -e "\e[42m       \e[0m\e[47m       \e[0m\e[41m       \e[0m"
-echo -e "\e[42m       \e[0m\e[47m       \e[0m\e[41m       \e[0m"
-echo -e "\e[42m       \e[0m\e[47m       \e[0m\e[41m       \e[0m"
-echo -e "\e[42m       \e[0m\e[47m       \e[0m\e[41m       \e[0m"
-echo -e "\e[42m       \e[0m\e[47m       \e[0m\e[41m       \e[0m"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m' 
 
 
-
+echo -e "${GREEN}      _     _           _       ${NC}"
+echo -e "${YELLOW}     | |   (_)         | |      ${NC}"
+echo -e "${BLUE}     | |__  _ _ __   __| | ___  ${NC}"
+echo -e "${MAGENTA}     | '_ \| | '_ \ / _\` |/ _ \ ${NC}"
+echo -e "${CYAN}     | | | | | | | | (_| | (_) |${NC}"
+echo -e "${RED}     |_| |_|_|_| |_|\__,_|\___/ ${NC}"
+echo -e "                                       "
+echo -e "${GREEN}           J U N G L E          ${NC}"
 
 echo -e "${GREEN}##########################################################${NC}"
 echo -e "${GREEN}#${NC}                                                      ${GREEN}#${NC}"
